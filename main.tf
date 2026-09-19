2339e2f3e233956460f9404889fa8d0185bde076
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2026-09-19 20:58:05 +0530

    destroy s3 bucket

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "qs-en-${var.environment}-test-bucket"
+# resource "aws_s3_bucket" "example" {
+#   bucket = "qs-en-${var.environment}-test-bucket"
 
-  tags = {
-    Name        = "qs-en-${var.environment}-test-bucket"
-    Environment = "development"
-  }
-}
+#   tags = {
+#     Name        = "qs-en-${var.environment}-test-bucket"
+#     Environment = "development"
+#   }
+# }
 
 
 variable "environment" {
