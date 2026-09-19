509e1d30911b25fdc08d2e891d7eb46b2601149f
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2026-09-19 20:55:15 +0530

    updated s3 bucket

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "my-qs-test-bucket"
+  bucket = "qs-en-${var.environment}-test-bucket"
 
   tags = {
     Name        = "qs-en-${var.environment}-test-bucket"
